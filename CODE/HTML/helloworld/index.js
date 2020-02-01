14e5e8ed4da1beffd1303829b58f8601e46e1482
pho0575 <pho0570@gmail.com>
2020-02-01 15:47:49 +0900

    calculator

--- CODE/HTML/helloworld/index.js
+++ CODE/HTML/helloworld/index.js
@@ -1,1 +1,27 @@
-console.log('Im Working Im JS. Im Beatutiful. Im worth it');
+const calculator = {
+    plus: function(a, b){
+      return a + b;
+    },
+    multiplication: function(a, b){
+      return a * b;
+    },
+    division: function(a, b){
+      return a / b;
+    },
+    minus: function(a, b){
+      return a - b;
+    },
+    power: function(a, b){
+      return a ** b;
+    }
+    
+}
+console.log(calculator.plus(5, 5))
+const multiplication = calculator.multiplication(5, 5)
+const division = calculator.division(5, 5)
+const minus = calculator.minus(5, 5)
+const power = calculator.power(5, 5)
+  
+  
+  console.log(plus, multiplication, division, minus, power);
+  
