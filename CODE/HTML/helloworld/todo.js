b96c12e1df23995e473d6f0b75cace5b931624ee
pho0575 <pho0570@gmail.com>
2020-02-06 19:41:02 +0900

    ToDolist

---
+++ CODE/HTML/helloworld/todo.js
@@ -0,0 +1,55 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList"),
+    TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "X";
+    const span = document.createElement("span");
+    span.innerText = text;
+    const blank = document.createElement("blank");
+    blank.innerText = "  ";
+    const newId = toDos.length +1;
+    li.appendChild(span);
+    li.appendChild(blank);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text : text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS)
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        })
+            
+        };
+    }
+
+
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit)
+}
+init();
