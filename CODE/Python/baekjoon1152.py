41cb2002b1d12adb446c73f915499d7be63c038b
pho0575 <pho0570@gmail.com>
2020-03-04 12:30:57 +0900

    correct

---
+++ CODE/Python/baekjoon1152.py
@@ -0,0 +1,3 @@
+arr=list(input().split())
+arr.append('>>')
+print(arr.index('>>')) #뭔가 코드가 이상하다..ㅋㅋㅋ
--- CODE/Python/baekjoon1157.py
+++ CODE/Python/baekjoon1157.py
@@ -3,7 +3,7 @@ count=[]
 whatif=0
 for b in range(len(word)):
     if ord(word[b]) >= 97:
-        word[b]=chr(ord(word[b])-32)
+        word[b]=chr(ord(word[b])-32) # upper()썻으면 더 좋았을 것
 for a in range(65, 91):
     count.append(word.count(chr(a)))
 for b in range(25):
