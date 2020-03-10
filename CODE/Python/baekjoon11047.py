de10a628796c6fcbd8f6f9becbec48f3f9c58d47
pho0575 <pho0570@gmail.com>
2020-03-10 12:40:13 +0900

    baekjoon

--- CODE/Python/baekjoon11047.py
+++ CODE/Python/baekjoon11047.py
@@ -1,25 +1,17 @@
 n, k=map(int, input().split())
 arr=[]
-count=0
 num=0
-complete=1
 for a in range(n):
   arr.append(int(input()))
-arr.reverse()
-for a in range(n):
-  while True:
-    num+=arr[a]
-    if num == k:
-      complete=0
-      count+=1
-      break
-    elif num > k:
-      num-=arr[a]
-      break
-    count+=1
-  if complete == 0:
+for a in range(n-1, -1, -1):
+  if k == 0:
     break
-print(count)
+  if arr[a] > k:
+    continue
+  num+= k//arr[a]
+  k%=arr[a]
+  
+print(num)
 #밑에서 부터 더한다
 #더했는데 k 보다 크면 다시 빼고 다음 값을 더한다
 #k 와 값이 같아질때까지 count를 올리면서 반복문을 실행한다
---
+++ CODE/Python/baekjoon1931.py
@@ -0,0 +1,27 @@
+# 조건 
+# 회의는 한번 시작하면 종료할수 없다
+# 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다
+# 회의의 시작 시간과 끝나는 시간이 같을수도 있다.
+# 결정사항
+# 내부적으로 파이썬에 내장되어있는 sort 함수의 시간 복잡도는 평균적으로 nlogn이므로 별도의 정렬 메소드를 다시 짤 필요는 없다.
+# 풀이 방법
+# 기존에 정렬이 된 리스트를 파이썬 내부정렬 함수를 이용해 문제에서 주어진 기준으로 재정렬을 한다면, 기존 기준의 결과값을 최대한 유지하면서 정렬되도록 한다. 이문제에서는 시작시간과 종료시간이 일치하더라도 회의실 배정이 가능하다는 조건이있다. 즉 (1,2) (2, 2)는 모두 회의실을 배정 가능하다는 말이다
+import sys
+n=int(sys.stdin.readline())
+arr=[]
+for a in range(n):
+  arr.append(list(map(int, sys.stdin.readline().split())))
+arr=sorted(arr, key=lambda x:x[0])
+arr=sorted(arr, key=lambda x:x[1])
+for a in range(1, n):
+  try:
+    while True:
+      if arr[a][0] < arr[a-1][1]:
+        del arr[a]
+        continue
+      else:
+        break
+  except:
+    break
+arr.append('>>')
+print(arr.index('>>'))
