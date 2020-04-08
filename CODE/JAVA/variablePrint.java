f2e6ce9a5f209040c2fde9de3b179f743e7543fe
pho0575 <pho0570@gmail.com>
2020-04-09 01:31:09 +0900

    commit

---
+++ CODE/JAVA/variablePrint.java
@@ -0,0 +1,23 @@
+public class variablePrint {// public 클래스 생성, 클래스명 HelloJava
+	    public static void main(String[] args) {
+        int a = 0206; //정수는 int 로 선언
+        long b = 1500000L; //long 타입은 long으로 선언
+        char c ='A'; //문자 선언은 char
+        String d ="컴퓨터"; // 문자열 선언은 String *대문자 사용에 주의
+        String e ="내가 작성한 \"자바\" 프로그램"; // 큰 따옴표를 작성하기 위해 \"를 사용
+        double f = 3.141592653589793; //float 범위를 초과해 double 사용
+        float g = 3500; //실수는 float로 선언
+
+        
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g); //문자열 함수 println을 사용해서 출력
+        
+    }
+ 
+}
--- CODE/Tensorflow/test.py
+++ CODE/Tensorflow/test.py
@@ -1,1 +1,32 @@
-import tensorflow as tf
+# def compare_string(a, b):
+#   if len(a) > len(b):
+#     return a
+#   else:
+#     return b
+
+# def game369(num):
+#   if num%3==0:
+#     return "짝!"
+#   elif "3" in str(num):
+#     return "짝!"
+#   else:
+#     return num
+  
+
+# def make_list(size):
+#   import random
+#   A=[]
+#   for i in range(size):
+#     A.append(random.randint(0,100))
+#   return A
+
+
+def random_pop(num):
+  import random
+  a=[i for i in range(num)]
+  random.shuffle(a)
+  for _ in range(num):
+    print(a.pop())
+
+k=100
+random_pop(k)
