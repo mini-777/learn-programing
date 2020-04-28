cc323ec974e5d88c55404ec15c9887186848af6d
pho0575 <pho0570@gmail.com>
2020-04-29 03:35:47 +0900

    commit

--- CODE/Python/test/test.py
+++ CODE/Python/test/test.py
@@ -1,32 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 import time
+form_class = uic.loadUiType("untitled.ui")[0]
 
-while 1:
-    time.sleep(0.1)
-    driver = webdriver.Chrome()
-    url = 'http://cafe.daum.net/breakjob/YRx/2526'
-    driver.get(url)
-    action = ActionChains(driver)
-    driver.close()
-    time.sleep(0.1)
-    driver = webdriver.Chrome()
-    url = 'http://cafe.daum.net/breakjob/8paQ/24'
-    driver.get(url)
-    action = ActionChains(driver)
-    driver.close()
-    time.sleep(0.1)
-    driver = webdriver.Chrome()
-    url = 'http://cafe.daum.net/breakjob/8SdF/1749'
-    driver.get(url)
-    action = ActionChains(driver)
-    driver.close()
-    time.sleep(0.1)
-    driver = webdriver.Chrome()
-    url = 'http://cafe.daum.net/breakjob/8SdF/1747'
-    driver.get(url)
-    action = ActionChains(driver)
-    driver.close()
 
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.startFunction)
+        self.pushButton_2.clicked.connect(self.addItem)
+        self.pushButton_3.clicked.connect(self.removeCurrentItem)
+    def addItem(self):
+        if not self.lineEdit_3.text()=="":
+            self.listWidget.addItem(self.lineEdit_3.text())
+            self.lineEdit_3.setText("")
+    def startFunction(self) :
+        for count in range(self.listWidget.count()):
+            arr=[]
+            arr.append(self.listWidget.item(count).text())
+        for _ in range(self.spinBox_2.value()):
+            for url in arr:
+                time.sleep(self.doubleSpinBox.value())
+                driver = webdriver.Chrome()
+                driver.get(url)
+                action = ActionChains(driver)
+                driver.close()
+    def removeCurrentItem(self) :
+       
+        self.removeItemRow = self.listWidget.currentRow()
+        self.listWidget.takeItem(self.removeItemRow)
+
+
+        
+if __name__ == "__main__" :
+
+    app = QApplication(sys.argv) 
+
+
+    myWindow = WindowClass() 
+
+
+    myWindow.show()
+
+
+    app.exec_()
